7530f3184c9b3a465a6a0fa8f7b5e14dcdc33068
Favour <ofavourite001@gmail.com>
2024-01-20 23:12:03 +0000

    101-

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,6 +8,17 @@ if  __name__ == "__main__":
     if argc < 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if sys.argv[2] not in ["+", "-", "*", "/"]:
+    if sys.argv[2] not in ["*", "+", "-", "/"]:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if sys.argv[2] == "+":
+        c = add(a, b)
+    elif sys.argv[2] == "-":
+        c = sub(a, b)
+    elif sys.argv[2] == "*":
+        c = mul(a, b)
+    elif sys.argv[2] == "/":
+        c = div(a, b)
+    print("{} {} {} = {}".format(a, sys.argv[2], b, c))
