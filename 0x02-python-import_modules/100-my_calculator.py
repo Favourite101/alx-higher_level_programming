0591efc84553beabcc8e8d868b96d9ef63475751
Favour <ofavourite001@gmail.com>
2024-01-21 14:51:28 +0100

    VS

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if  __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
     argc = len(sys.argv)
-    if argc != 4:
+    if argc < 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     if sys.argv[2] not in ["*", "+", "-", "/"]:
