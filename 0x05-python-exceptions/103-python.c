f46f45333710b3ea2ff6a169ef1587f65a38eff9
Favour <ofavourite001@gmail.com>
2024-02-06 12:29:27 +0100

    Commit

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -6,8 +6,8 @@
  */
 void print_python_float(PyObject *p)
 {
-	double value = 0;
-	char *string = NULL;
+	double num = 0;
+	char *s = NULL;
 
 	fflush(stdout);
 	printf("[.] float object info\n");
@@ -17,9 +17,9 @@ void print_python_float(PyObject *p)
 		printf("  [ERROR] Invalid Float Object\n");
 		return;
 	}
-	value = ((PyFloatObject *)p)->ob_fval;
-	string = PyOS_double_to_string(value, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
-	printf("  value: %s\n", string);
+	num = ((PyFloatObject *)p)->ob_fval;
+	s = PyOS_double_to_string(num, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
+	printf("  value: %s\n", s);
 }
 /**
  * print_python_bytes - gives data of the PyBytesObject
@@ -28,7 +28,7 @@ void print_python_float(PyObject *p)
 void print_python_bytes(PyObject *p)
 {
 	Py_ssize_t size = 0, i = 0;
-	char *string = NULL;
+	char *s = NULL;
 
 	fflush(stdout);
 	printf("[.] bytes object info\n");
@@ -39,12 +39,12 @@ void print_python_bytes(PyObject *p)
 	}
 	size = PyBytes_Size(p);
 	printf("  size: %zd\n", size);
-	string = (assert(PyBytes_Check(p)), (((PyBytesObject *)(p))->ob_sval));
-	printf("  trying string: %s\n", string);
+	s = (assert(PyBytes_Check(p)), (((PyBytesObject *)(p))->ob_sval));
+	printf("  trying string: %s\n", s);
 	printf("  first %zd bytes:", size < 10 ? size + 1 : 10);
 	while (i < size + 1 && i < 10)
 	{
-		printf(" %02hhx", string[i]);
+		printf(" %02hhx", s[i]);
 		i++;
 	}
 	printf("\n");
